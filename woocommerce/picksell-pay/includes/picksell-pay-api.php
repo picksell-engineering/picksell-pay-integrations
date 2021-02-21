714033a179d344268eab75c2786562ff4a472d01
qwertymypass <qwertymypass@yandex.ru>
2021-02-21 06:04:44 +0300

    PICK-836: handle webhook

--- woocommerce/picksell-pay/includes/picksell-pay-api.php
+++ woocommerce/picksell-pay/includes/picksell-pay-api.php
@@ -1,54 +1,60 @@
 <?php
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
 	exit;
 }
 
 class WC_PicksellPay_API {
-	const SKD_URL_DEV		= 'https://sdk.psd2.club/orders';
-	const SKD_URL_PROD		= 'https://sdk.psd2.club/orders';	// todo: change on prod url
-	const SHOPPING_URL_DEV	= 'https://shopping.psd2.club';
-	const SHOPPING_URL_PROD	= 'https://shopping.psd2.club';		// todo: change on prod url
+	const SKD_URL_DEV = 'https://sdk.psd2.club/orders';
+	const SKD_URL_PROD = 'https://sdk.psd2.club/orders'; // todo: change on prod url
+	const SHOPPING_URL_DEV = 'https://shopping.psd2.club';
+	const SHOPPING_URL_PROD = 'https://shopping.psd2.club'; // todo: change on prod url
 
 	private static $secret_key = '';
 	private static $dev_mode = true;
 
-	public static function set_secret_key( $secret_key ) {
+	public static function set_secret_key($secret_key) {
 		self::$secret_key = $secret_key;
 	}
-	
+
 	public static function set_environment($dev_mode) {
 		self::$dev_mode = $dev_mode == 'yes' ? true : false;
 	}
 
 	public static function get_headers() {
 		return array(
-			'Authorization'	=> 'Basic ' . base64_encode(self::$secret_key),
-			'Content-Type'	=> 'application/json; charset=utf-8',
+			'Authorization' => 'Basic ' . base64_encode(self::$secret_key),
+			'Content-Type' => 'application/json; charset=utf-8',
 		);
 	}
 
 	public static function createOrder($order) {
+		/*
+			todo:
+			1. check available currency
+			2. add callback url and return url
+			3. make description?
+		*/
 		$request = array(
-			'totalAmount'	=> $order->get_total(),
-			'currency'		=> 'EUR',
-			'description'	=> 'WC Order id ' . $order->get_id(),
+			'totalAmount' => $order->get_total(),
+			'currency' => 'EUR',
+			'description' => 'WC Order id ' . $order->get_id(),
 		);
 
 		$response = wp_remote_post(
 			self::get_sdk_url(),
 			array(
-				'method'	=> 'POST',
-				'headers'	=> self::get_headers(),
-				'body'   	=> json_encode($request),
-				'timeout'	=> 15,
+				'method' => 'POST',
+				'headers' => self::get_headers(),
+				'body' => json_encode($request),
+				'timeout' => 15,
 			)
 		);
 
-		$response_body = json_decode( $response['body'] );
+		$response_body = json_decode($response['body']);
 
 		if (property_exists($response_body, 'payload')) {
-            return $response_body->payload->id;
-        }
+			return $response_body->payload->id;
+		}
 
 		return null;
 	}
@@ -56,8 +62,8 @@ class WC_PicksellPay_API {
 	public static function get_sdk_url() {
 		return self::$dev_mode ? self::SKD_URL_DEV : self::SKD_URL_PROD;
 	}
-	
+
 	public static function get_order_page_url($picksell_order_id) {
-		return (self::$dev_mode ? self::SHOPPING_URL_DEV : self::SHOPPING_URL_PROD) . '/orders/' .$picksell_order_id;
+		return (self::$dev_mode ? self::SHOPPING_URL_DEV : self::SHOPPING_URL_PROD) . '/orders/' . $picksell_order_id;
 	}
 }
--- woocommerce/picksell-pay/includes/picksell-pay-gateway.php
+++ woocommerce/picksell-pay/includes/picksell-pay-gateway.php
@@ -1,110 +1,81 @@
 <?php
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
 	exit;
 }
 
-function pp_init_gateway_class() {
-    if ( !class_exists( 'WC_Payment_Gateway' ) ) {
-        return;
-    }
+class WC_Gateway_Picksell_Pay extends WC_Payment_Gateway {
+	public function __construct() {
+		$this->id = 'picksell-pay';
+		$this->has_fields = false;
 
-    if ( class_exists( 'WC_Gateway_Picksell_Pay' ) ) {
-        return;
-    }
+		$this->method_title = 'Picksell Pay';
+		$this->method_description = 'Accept payment using Picksell.Pay';
 
-    class WC_Gateway_Picksell_Pay extends WC_Payment_Gateway {
-        public function __construct() {
-            $this->id = 'picksell-pay';
-            $this->has_fields = false;
+		$this->init_form_fields();
+		$this->init_settings();
 
-            $this->method_title = 'Picksell Pay';
-            $this->method_description = 'Accept payment using Picksell.Pay';
+		$this->title = $this->get_option('title');
+		$this->secret_key = $this->get_option('secret_key');
+		$this->dev_mode = $this->get_option('dev_mode');
 
-            $this->init_form_fields();
-            $this->init_settings();
+		WC_PicksellPay_API::set_secret_key($this->secret_key);
+		WC_PicksellPay_API::set_environment($this->dev_mode);
 
-            $this->title = $this->get_option( 'title' );
-            $this->secret_key = $this->get_option( 'secret_key' );
-            $this->dev_mode = $this->get_option( 'dev_mode' );
-            
-            WC_PicksellPay_API::set_secret_key( $this->secret_key );
-            WC_PicksellPay_API::set_environment( $this->dev_mode );
+		$this->form_fields = array(
+			'enabled' => array(
+				'title' => __('Enable/Disable', 'woocommerce'),
+				'type' => 'checkbox',
+				'label' => 'Enable payment with Picksell.Pay',
+				'default' => 'yes',
+				'description' => '<a href="https://picksell.eu" target="_blank">Additional information</a>',
+			),
+			'title' => array(
+				'title' => __('Title', 'woocommerce'),
+				'type' => 'text',
+				'description' => __('This controls the title which the user sees during checkout.', 'woocommerce'),
+				'default' => 'Picksell Pay',
+				'desc_tip' => true,
+			),
+			'secret_key' => array(
+				'title' => 'Merchant token',
+				'description' => 'You can get a token in your personal account',
+				'type' => 'text',
+				'default' => '',
+			),
+			'dev_mode' => array(
+				'title' => __('Enable/Disable', 'woocommerce'),
+				'type' => 'checkbox',
+				'label' => 'Use DEV environment on Picksell Pay',
+				'default' => 'no',
+				'description' => 'Use for tests only ',
+			),
+		);
 
-            $this->form_fields = array(
-                'enabled'   => array(
-                    'title'       => __( 'Enable/Disable', 'woocommerce' ),
-                    'type'        => 'checkbox',
-                    'label'       => 'Enable payment with Picksell.Pay',
-                    'default'     => 'yes',
-                    'description' => '<a href="https://picksell.eu" target="_blank">Additional information</a>',
-                ),
-                'title'     => array(
-                    'title'       => __( 'Title', 'woocommerce' ),
-                    'type'        => 'text',
-                    'description' => __( 'This controls the title which the user sees during checkout.', 'woocommerce' ),
-                    'default'     => 'Picksell Pay',
-                    'desc_tip'    => true,
-                ),
-                'secret_key'  => array(
-                    'title'       => 'Merchant token',
-                    'description' => 'You can get a token in your personal account',
-                    'type'        => 'text',
-                    'default'     => '',
-                ),
-                'dev_mode'   => array(
-                    'title'       => __( 'Enable/Disable', 'woocommerce' ),
-                    'type'        => 'checkbox',
-                    'label'       => 'Use DEV environment on Picksell Pay',
-                    'default'     => 'no',
-                    'description' => 'Use for tests only ',
-                ),
-            );
+		add_action('woocommerce_update_options_payment_gateways_' . $this->id, array($this, 'process_admin_options'));
+	}
 
-            add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
-        }
+	public function process_payment($order_id) {
+		$order = new WC_Order($order_id);
+		$picksell_order_id = WC_PicksellPay_API::createOrder($order);
 
-        public function process_payment( $order_id ) {
-            $order = new WC_Order( $order_id );
-            $picksell_order_id = WC_PicksellPay_API::createOrder($order);
+		if (!$picksell_order_id) {
+			return array(
+				'result' => 'fail',
+				'redirect' => '',
+			);
+		};
 
-            if (!$picksell_order_id) {
-                return array(
-                    'result'   => 'fail',
-                    'redirect' => '',
-                );
-            };
+		$order->update_status('pending', 'Payment pending');
 
-            $order->update_status( 'pending', 'Payment pending' );
+		$order->update_meta_data('_picksell_pay_order_id', $picksell_order_id, true);
+		$order->save();
 
-            $order->update_meta_data( '_picksell_pay_order_id', $picksell_order_id, true );
-            $order->save();
+		WC()->cart->empty_cart();
 
-            WC()->cart->empty_cart();
-
-            return array(
-                'result'   => 'success',
-                'redirect' => WC_PicksellPay_API::get_order_page_url($picksell_order_id),
-            );
-        }
-
-        public static function get_order_by_picksell_id( $picksell_order_id ) {
-            global $wpdb;
-    
-            $order_id = $wpdb->get_var( $wpdb->prepare( "SELECT DISTINCT ID FROM $wpdb->posts as posts LEFT JOIN $wpdb->postmeta as meta ON posts.ID = meta.post_id WHERE meta.meta_value = %s AND meta.meta_key = %s", $picksell_order_id, '_picksell_pay_order_id' ) );
-    
-            if ( ! empty( $order_id ) ) {
-                return wc_get_order( $order_id );
-            }
-    
-            return false;
-        }
-    }
-    
-    add_filter( 'woocommerce_payment_gateways', function ( $methods ) {
-        $methods[] = 'WC_Gateway_Picksell_Pay';
-        return $methods;
-    } );
+		return array(
+			'result' => 'success',
+			'redirect' => WC_PicksellPay_API::get_order_page_url($picksell_order_id),
+		);
+	}
 }
-
-add_action( 'plugins_loaded', 'pp_init_gateway_class' );
